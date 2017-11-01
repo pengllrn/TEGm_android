cccfd6e29bb08995ba2045c48febb86f41519744
pengllrn <1256880407@qq.com>
2017-11-01 22:14:21 +0800

    第二次上传 修改了设备查询界面的设计  增加了查询放大镜、 第三次 完成了筛选 第四次  增加了报废申请的UI 第五次 完成了报废申请的功能实现 第六次 新增报废列表查看

---
+++ app/src/main/java/com/pengllrn/tegm/activity/DamageApplyList.java
@@ -0,0 +1,15 @@
+package com.pengllrn.tegm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pengllrn.tegm.R;
+
+public class DamageApplyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_damage_apply_list);
+    }
+}
